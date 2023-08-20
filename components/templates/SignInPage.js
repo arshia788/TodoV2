9dbd05c16bafc8f29b5e95a34391418f78888be7
Amirali Amanipour <arshia.ala80@gmail.com>
2023-08-20 12:17:30 +0330

    signin

--- components/templates/SignInPage.js
+++ components/templates/SignInPage.js
@@ -1,61 +1,56 @@
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import { signIn, useSession } from 'next-auth/react'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/router';
+import {signIn, useSession} from 'next-auth/react'
 
-function SignUpPage() {
+function SignInPage() {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const { status } = useSession();
     const router = useRouter();
 
-    useEffect(() => {
+    const {status} = useSession();
 
-        if (status === 'authenticated') window.location.href('/')
 
-    }, [status])
+    useEffect(()=>{
+        if(status === 'authenticated')  router.replace('/')
+
+    },[status])
 
     const signInHandler = async () => {
-        const res = await signIn('credentials', {
-            email, password, redirect: false
+        const res= await signIn('credentials', {
+            email, password, redirect:false
         });
         console.log(res);
-        if (!res.error) router.push('/')
+        if(!res.error)  router.push('/')
 
     }
 
     return (
-        <div>
+        <div >
 
-            <div className='bg-white rounded p-2 box-border flex flex-col w-3/12 mx-auto my-20'>
-                <h2 className='text-lg text-center font-semibold text-blue-700'>SignIn-Page</h2>
+            <div className='bg-white w-64 mx-auto rounded-lg 
+             p-5 flex flex-col text-center my-10'>
 
-                <input
-                    className='shadow-md border-none outline-none shadow-gray-600 p-2 rouded my-4'
-                    value={email} onChange={e => setEmail(e.target.value)} placeholder='Email...' />
+                <h3 className='text-md mb-3 font-semibold'>SignIn</h3>
 
-                <input
-                    className='shadow-md border-none outline-none shadow-gray-600 p-2 rouded my-4'
-                    value={password} onChange={e => setPassword(e.target.value)} placeholder='Password...' />
+                <input onChange={e => setEmail(e.target.value)} className='shadow-md outline-none shadow-gray-400 my-2 p-2 rounded' type='text' placeholder='email' value={email} />
+                <input onChange={e => setPassword(e.target.value)} className='shadow-md outline-none shadow-gray-400 my-2 p-2 rounded' type='password' placeholder='password...' value={password} />
 
                 <button
                     onClick={signInHandler}
-                    className='bg-gray-400 w-fit px-2 py-1 rounded mt-4 mx-auto'
-                >SignIn</button>
+                    className='px-3 w-fit mt-4 mx-auto py-1 rounded bg-zinc-400'
+                >LogIn</button>
 
                 <div className='flex items-center mt-4'>
                     <p className='mr-1'>Create An Account </p>
                     <Link href="/signup" className='text-blue-600'>sign up</Link>
                 </div>
 
-
-
             </div>
-
         </div>
     )
 }
 
-export default SignUpPage;
+export default SignInPage
